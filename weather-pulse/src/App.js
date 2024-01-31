6235dde50c30a6e0f8b8c87fa5bb0f09c86d1231
Prashant-Aher <prashantaher2112@gmail.com>
2024-01-31 11:49:07 +0530

    fetched weather data from OpenWeatherMap

--- weather-pulse/src/App.js
+++ weather-pulse/src/App.js
@@ -1,6 +1,36 @@
 import './App.css';
+import React, { useEffect, useState } from "react";
+
 
 function App() {
+
+  const [data, setData] = useState([null]);
+  const [lat, setLat] = useState([null]);
+  const [long, setLong] = useState([null]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      navigator.geolocation.getCurrentPosition(function (position) {
+        setLat(position.coords.latitude);
+        setLong(position.coords.longitude);
+      });
+
+
+      if (lat !== null && long !== null) {
+        const apiUrl = `${process.env.REACT_APP_API_URL}/weather/?lat=${lat}&lon=${long}&units=metric&APPID=${process.env.REACT_APP_API_KEY}`;
+        const response = await fetch(apiUrl);
+        const result = await response.json();
+        setData(result);
+        console.log(apiUrl);
+        console.log("Latitude is:", lat);
+        console.log("Longitude is:", long);
+        console.log(result);
+      }
+    };
+    fetchData();
+  }, [lat,long])
+
+
   return (
     <div className="App">
 
